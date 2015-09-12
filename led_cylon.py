ae78a3fa6112a9301f773fa7dd87c7d8259f43a8
Dan Stoner <danstoner@gmail.com>
2015-09-11 20:22:53 -0400

    added speed switch and turn off leds on interrupt

--- led_cylon.py
+++ led_cylon.py
@@ -2,37 +2,54 @@
 # This is based on the led_blink.py example that came with the GoPiGo
 # to blink the LEDs
 
-## TODO:
-## 1. add command-line parameter to control blink speed
-## 2. catch exceptions to make sure we are turning off LEDs at
-###   program termination (rather than leaving them in last on/off state)
-
 from gopigo import *
 import sys
 import time
+import argparse
 
-while True:
-    print "."
+argparser = argparse.ArgumentParser(description='Script to activate GoPiGo LEDs in Cylon visor fashion.')
+argparser.add_argument(-s,"--speed", type=int, help="A number between 1 and 10 to choose the speed of LED on and off action. 1 is fastest, 10 is slowest.")
+args = argparser.parse_args()
+
+# default speed
+speed = 5
+
+if 1 <= args.speed <= 10: 
+    speed = args.speed
+        
+delay = .01*speed*speed
+
+# We wrap the loop with try/except in order to make sure that we turn off the
+# LEDs when the script is interrupted with ctrl-c
+
+try:
+    while True:
+    print ".",
     led_on(LED_L)
-    time.sleep(.5)
+    time.sleep(delay)
     led_on(LED_R)
-    time.sleep(.5)
+    time.sleep(delay)
     print "."
     led_off(LED_L)
-    time.sleep(.5)
+    time.sleep(delay)
     led_off(LED_R)
-    time.sleep(.5)
+    time.sleep(delay)
 
 
-    print "."
+    print ".",
     led_on(LED_R)
-    time.sleep(.5)
+    time.sleep(delay)
     led_on(LED_L)
-    time.sleep(.5)
+    time.sleep(delay)
     print "."
     led_off(LED_R)
-    time.sleep(.5)
+    time.sleep(delay)
     led_off(LED_L)
     time.sleep(.5)
-
+except KeyboardInterrupt:
+    print "Turning off LEDs and shutting down..."
+    led_off(LED_L)
+    led_off(LED_R)
+    raise SystemExit
+                        
     
